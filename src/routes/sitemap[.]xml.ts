a994940e72f9a886a2a7610985047d7924b7b3bb
triyad2009 <tah.riyad.tr@gmail.com>
2026-09-20 00:43:11 +0600

    SEO: use production domain martellocup.mvp.bd

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import type {} from "@tanstack/react-start";
 
-const BASE_URL = "https://martellocup.lovable.app";
+const BASE_URL = "https://martellocup.mvp.bd";
 
 interface SitemapEntry {
   path: string;
